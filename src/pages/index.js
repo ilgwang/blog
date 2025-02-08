8e34fa3081ad9c2ab58e36e7ce8f80fede66dea1
ilgwang <ig95@dgu.ac.kr>
2025-02-08 20:41:04 +0900

    메인 화면 수정
    메인 화면 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useRef } from "react";
 import Layout from "@theme/Layout";
 import index from "./index.module.css";
@@ -23,56 +24,58 @@ function Home() {
   return (
     <Layout title="Home">
       <header>
-          <div className={index.indexHome} >
-              <h3>Daily Schedule</h3>
-              <div>
-                  <ul>
-                      <li className={index.schedule}>
-                          <div className={index.scheduleHeader}>침구 정리 및 운동 준비</div>
-                          <div className={index.scheduleBody}>5:00 am ~ 05:20 am</div>
-                          <div className={index.verticalLine}></div> 
-                      </li>
-                      <li className={index.schedule}>
-                          <div className={index.scheduleHeader}>홈 트레이닝</div>
-                          <div className={index.scheduleBody}>05:20 am ~ 06:00 am</div>
-                          <div className={index.verticalLine2}></div> 
-                      </li>
-                      <li className={index.schedule}>
-                          <div className={index.scheduleHeader}>자기 개발 진행</div>
-                          <div className={index.scheduleBody}>06:00 am ~ 08:00 am</div>
-                          <div className={index.verticalLine3}></div> 
-                      </li>
-                      <li className={index.schedule}>
-                          <div className={index.scheduleHeader}>출근 준비</div>
-                          <div className={index.scheduleBody}>08:00 am ~ 08:20 am</div>
-                          <div className={index.verticalLine4}></div> 
-                      </li>
-                      <li className={index.schedule}>
-                          <div className={index.scheduleHeader}>출근 및 회사 업무 진행</div>
-                          <div className={index.scheduleBody}>09:00 am ~ </div>
-                          <div className={index.verticalLine5}></div> 
-                      </li>
-                  </ul>
-              </div>
-          </div>
-          <div className={index.calendarIcon}>
-              <div>
-                  안녕하세요. <br/><span id={index.profileName}> 최일광</span> 입니다.
-              </div>
-              <div>
-                  주니어 웹 개발자로서 끊임없이 배우고 성장하고 있습니다. 
-              </div>
-              <div className={index.githubImage}>
-                  <a href="https://github.com/ilgwang" target="_blank" rel="noopener noreferrer" title="GitHub">
-                      <img src={githubImage} className={index.profileStyle} />
-                  </a>
-                  <a href="mailto:ig95@gmail.com" title="Gmail">
-                      <img src={gmailImage} className={index.profileStyle2} />
-                  </a>
-                  <Link to="/docs">
-                      <img src={myInfoImage} className={index.profileStyle3} />
-                  </Link>
-              </div>
+        <div className={index.multi_bg_example}>
+            <div className={index.indexHome} >
+                <h3>Daily Schedule</h3>
+                <div>
+                    <ul>
+                        <li className={index.schedule}>
+                            <div className={index.scheduleHeader}>침구 정리 및 운동 준비</div>
+                            <div className={index.scheduleBody}>5:00 am ~ 05:20 am</div>
+                            <div className={index.verticalLine}></div> 
+                        </li>
+                        <li className={index.schedule}>
+                            <div className={index.scheduleHeader}>홈 트레이닝</div>
+                            <div className={index.scheduleBody}>05:20 am ~ 06:00 am</div>
+                            <div className={index.verticalLine2}></div> 
+                        </li>
+                        <li className={index.schedule}>
+                            <div className={index.scheduleHeader}>자기 개발 진행</div>
+                            <div className={index.scheduleBody}>06:00 am ~ 08:00 am</div>
+                            <div className={index.verticalLine3}></div> 
+                        </li>
+                        <li className={index.schedule}>
+                            <div className={index.scheduleHeader}>출근 준비</div>
+                            <div className={index.scheduleBody}>08:00 am ~ 08:20 am</div>
+                            <div className={index.verticalLine4}></div> 
+                        </li>
+                        <li className={index.schedule}>
+                            <div className={index.scheduleHeader}>출근 및 회사 업무 진행</div>
+                            <div className={index.scheduleBody}>09:00 am ~ </div>
+                            <div className={index.verticalLine5}></div> 
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className={index.calendarIcon}>
+                <div>
+                    안녕하세요. <br/><span id={index.profileName}> 최일광</span> 입니다.
+                </div>
+                <div>
+                    주니어 웹 개발자로서 끊임없이 배우고 성장하고 있습니다. 
+                </div>
+                <div className={index.githubImage}>
+                    <a href="https://github.com/ilgwang" target="_blank" rel="noopener noreferrer" title="GitHub">
+                        <img src={githubImage} className={index.profileStyle} />
+                    </a>
+                    <a href="mailto:ig95@gmail.com" title="Gmail">
+                        <img src={gmailImage} className={index.profileStyle2} />
+                    </a>
+                    <Link to="/docs">
+                        <img src={myInfoImage} className={index.profileStyle3} />
+                    </Link>
+                </div>
+            </div>
           </div>
       </header>
       <main id="main" ref={mainRef} hidden={isLoading}>
